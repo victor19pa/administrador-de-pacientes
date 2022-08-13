79bea2afc66aaa253a8f3540a87099c4fd03ed16
victor19pa <vpinedaa@unah.hn>
2022-08-13 16:54:35 -0600

    finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,53 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Cita from "./components/Cita";
+import Formulario from "./components/Formulario";
 
 const App = () => {
+  let citasIniciales = JSON.parse(localStorage.getItem("citas"));
+
+  if (!citasIniciales) {
+    citasIniciales = [];
+  }
+
+  const [citas, setCitas] = useState([]);
+
+  useEffect(() => {
+    if (citasIniciales) {
+      localStorage.setItem("citas", JSON.stringify(citas));
+    } else {
+      localStorage.setItem("citas", JSON.stringify([]));
+    }
+  }, [citas]);
+
+  const crearCita = (cita) => {
+    setCitas([...citas, cita]);
+  };
+
+  const eliminarCita = (id) => {
+    const nuevasCitas = citas.filter((cita) => cita.id !== id);
+    setCitas(nuevasCitas);
+  };
+
+  const titulo = citas.length === 0 ? "No hay citas" : "Administra tus Citas";
+
   return (
-    <div>
-      <section>assas</section>
-    </div>
+    <>
+      <h1>Administrador de Pacientes</h1>
+
+      <div className="container">
+        <div className="row">
+          <div className="one-half column">
+            <Formulario crearCita={crearCita} />
+          </div>
+          <div className="one-half column">
+            <h2>{titulo}</h2>
+            {citas.map((cita) => (
+              <Cita key={cita.id} cita={cita} eliminarCita={eliminarCita} />
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
---
+++ src/components/Formulario.jsx
@@ -0,0 +1,116 @@
+import React, { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+
+const Formulario = ({ crearCita }) => {
+  //! crear state de citas
+  const [cita, setCita] = useState({
+    mascota: "",
+    propietario: "",
+    fecha: "",
+    hora: "",
+    sintomas: "",
+  });
+
+  const [error, setError] = useState(false);
+
+  const actualizarState = (e) => {
+    setCita({
+      ...cita,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const { mascota, propietario, fecha, hora, sintomas } = cita;
+
+  const submitCita = (e) => {
+    e.preventDefault();
+
+    //! validar
+    if (
+      mascota.trim() === "" ||
+      propietario.trim() === "" ||
+      fecha.trim() === "" ||
+      hora.trim() === "" ||
+      sintomas.trim() === ""
+    ) {
+      setError(true);
+      return;
+    }
+
+    setError(false);
+    //! asignar id
+    cita.id = uuidv4();
+    //! crear cita
+    crearCita(cita);
+    //! reiniciar form
+    setCita({
+      mascota: "",
+      propietario: "",
+      fecha: "",
+      hora: "",
+      sintomas: "",
+    });
+  };
+
+  return (
+    <>
+      <h2>Crear Citas</h2>
+      {error ? (
+        <p className="alerta-error">Todos los campos son obligatorios</p>
+      ) : null}
+      <form onSubmit={submitCita}>
+        <label>Nombre Mascota</label>
+        <input
+          type="text"
+          name="mascota"
+          className="u-full-width"
+          placeholder="Nombre Mascota"
+          onChange={actualizarState}
+          value={mascota}
+        />
+
+        <label>Nombre Dueño</label>
+        <input
+          type="text"
+          name="propietario"
+          className="u-full-width"
+          placeholder="Nombre Dueño de Mascota"
+          onChange={actualizarState}
+          value={propietario}
+        />
+
+        <label>Fecha</label>
+        <input
+          type="date"
+          name="fecha"
+          className="u-full-width"
+          onChange={actualizarState}
+          value={fecha}
+        />
+
+        <label>Hora</label>
+        <input
+          type="time"
+          name="hora"
+          className="u-full-width"
+          onChange={actualizarState}
+          value={hora}
+        />
+
+        <label>Sintomas</label>
+        <textarea
+          className="u-full-width"
+          name="sintomas"
+          onChange={actualizarState}
+          value={sintomas}
+        ></textarea>
+
+        <button type="submit" className="u-full-width button-primary">
+          Agregar Citas
+        </button>
+      </form>
+    </>
+  );
+};
+
+export default Formulario;
